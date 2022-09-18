017cbaa503dc8f5d81e085190178df937f7f7b18
yeongsang2 <dudtkd1221@gmail.com>
2022-09-18 23:37:53 +0900

    [boj] 숨바꼭질3 13549

--- baekjoon/HideAndSeek3.java
+++ baekjoon/HideAndSeek3.java
@@ -1,16 +1,19 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class HideAndSeek3 {
 
     public static int n,k;
     public static int min = Integer.MAX_VALUE;
-    public static int[] time = new int[100001];
+    public static boolean[] visited = new boolean[100001];
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -20,4 +23,52 @@ public class HideAndSeek3 {
         bfs();
         System.out.println(min);
     }
-}
+
+    public static class Human{
+        int loc;
+        int time;
+
+        public Human(int loc, int time) {
+            this.loc = loc;
+            this.time = time;
+        }
+    }
+
+    public static void bfs(){
+
+        Queue<Human> queue = new LinkedList<>();
+
+        queue.add(new Human(n, 0));
+        int next;
+
+        while(!queue.isEmpty()){
+
+            Human poll = queue.poll();
+            int now = poll.loc;
+            int time = poll.time;
+
+            if(now==k){ //잡았을경우
+                min = Math.min(min, time);
+            }
+
+            next = now *2; // 0초후 2*X 위치로 이동
+            if(next < 100001 && !visited[next]){
+                visited[next] = true;
+                queue.add(new Human(next, time));
+            }
+
+            next = now -1; // 0초후 x-1 위치로 이동
+            if( next >= 0 && !visited[next]){
+                visited[next] = true;
+                queue.add(new Human(next, time+1));
+            }
+
+            next = now +1; // 0초후 x+1 위치로 이동
+            if( next < 100001 && !visited[next]){
+                visited[next] = true;
+                queue.add(new Human(next, time+1));
+            }
+
+            }
+        }
+ }
