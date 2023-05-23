3111a1890fd17d2bd144f794d269dc29e7ea6096
yeongsang2 <dudtkd1221@gmail.com>
2023-05-23 09:22:10 +0900

    [programmers] 기능개발

---
+++ programmers/featFunctino.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import java.util.*;
+
+public class featFunctino {
+    public static void main(String[] args) {
+        int[] progresses = {95, 90, 99, 99, 80, 99};
+        int[] speeds = {1, 1, 1, 1, 1, 1};
+        List<Integer> answer = new ArrayList<>();
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < progresses.length; i++) {
+            queue.add( (int) Math.ceil( ( 100.0 -  progresses[i] / speeds[i] )) );
+        }
+        while(!queue.isEmpty()){
+            int day = queue.poll();
+            int count = 1;
+            while(!queue.isEmpty() && day >= queue.peek()){
+                queue.poll();
+                count++;
+            }
+            answer.add(count);
+        }
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+
+    }
+}
