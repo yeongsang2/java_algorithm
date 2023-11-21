d9b5084ec999322719cb882abd3e4154de0c5a19
yeongsang2 <dudtkd1221@gmail.com>
2023-11-21 21:46:46 +0900

    [boj] 11000 강의실배정  - 난이도 : D5  - 유형 : Greedy, PriorityQueue
     Arrays.sort(arr, new Compartor<int[]>9) {
        @Override
        public int compare(int[] o1, int[] o2){
           return o1[0] - o2[0];
        }
     }
     sort는 default가 오름차순이기 때문에
     ( 선행 - 후행 ) 음수일경우 순서를 바꾸지 않는다는 것을 의미
     o1 - o2
     내림차순으로 할경우
     o2 - o1 반대로 -> 왜? 음수가 되어야지 자리를 바꾸지 않음

---
+++ baekjoon/boj_11000.java
@@ -0,0 +1,57 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.PriorityQueue;
+
+public class boj_11000 {
+
+        static class Subject implements Comparable<Subject>{
+
+        public Subject(int start, int end){
+            this.start = start;
+            this.end = end;
+        }
+        int start;
+        int end;
+
+            @Override
+            public int compareTo(Subject subject) {
+                return this.start > subject.start ? 1 : -1;
+            }
+        }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int answer = 1;
+        int[][] arr = new int[n][2];
+
+        Queue<Integer> queue = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            arr[i][0] = start;
+            arr[i][1] = end;
+        }
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0] == o2[1]) return o1[1] - o2[1];
+                return o1[0] - o2[0];
+            }
+        });
+
+        queue.add(arr[0][1]);
+        for(int i =1 ; i <n ;i++){
+            if(queue.peek() <= arr[i][0]){
+                queue.poll();
+            }
+            queue.add(arr[i][1]);
+        }
+        System.out.println(queue.size());
+    }
+}
