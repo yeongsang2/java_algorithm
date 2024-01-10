cb49edca4f8a46fde2b9886b7e631c086a724429
yeongsang2 <dudtkd1221@gmail.com>
2024-01-10 10:42:32 +0900

    [boj] 11403 경로 찾기   - 난이도 : silver 1   - 유형 : 최단거리   - 시간 : x

---
+++ baekjoon/boj_11403.java
@@ -0,0 +1,41 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_11403 {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int[][] map = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < n; i++) { // 거쳐가는 경로
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    if(map[j][i] == 1 && map[i][k] == 1){
+                        map[j][k] = 1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
