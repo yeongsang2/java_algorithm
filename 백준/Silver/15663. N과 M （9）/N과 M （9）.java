8c2d515409a30dc38ad0ec5d6cc16a35c8131397
Kim Yeong Sang <dudtkd1221@gmail.com>
2024-12-18 15:23:27 +0900

    [Silver II] Title: N과 M (9), Time: 420 ms, Memory: 37988 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,58 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static Set<String> set = new LinkedHashSet<>();
+    static int n;
+    static int m;
+    static boolean[] visited;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        visited = new boolean[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            visited[i] = true;
+            search(Integer.toString(arr[i]), i, 1);
+            visited[i] = false;
+        }
+
+
+        for(String s : set){
+            System.out.println(s);
+        }
+    }
+    static void search(String s, int now, int depth){
+        if(depth == m){
+            set.add(s);
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if(!visited[i]){
+                visited[i] = true;
+                search(s + " " + Integer.toString(arr[i]), i, depth+1);
+                visited[i] = false;
+            }
+
+        }
+
+
+    }
+}
