bacfe912256311f196fe36daf74733c84ed7ae5e
Kim Yeong Sang <dudtkd1221@gmail.com>
2025-02-13 19:57:36 +0900

    [Silver I] Title: 후보 추천하기, Time: 120 ms, Memory: 14584 KB -BaekjoonHub

--- 백준/Silver/1713. 후보 추천하기/후보 추천하기.java
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.java
@@ -1,15 +1,16 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+
     static class Info implements Comparable<Info> {
         int number;
         int time;
         int recommend;
-        
-        public Info(int number, int time) {
+        public Info(int number, int time){
             this.number = number;
             this.time = time;
             this.recommend = 1;
@@ -17,54 +18,53 @@ public class Main {
 
         @Override
         public int compareTo(Info o) {
-            if(this.recommend != o.recommend) {
+
+            if(this.recommend != o.recommend){
                 return this.recommend - o.recommend;
             }
-            return this.time - o.time;  // 시간이 작은(오래된) 것이 우선
+            return this.time - o.time;
         }
     }
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
-        
-        List<Info> photos = new ArrayList<>();
+
+        List<Info> list = new ArrayList<>();
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        for (int time = 0; time < m; time++) {
+        for (int i = 0; i < m; i++) {
             int num = Integer.parseInt(st.nextToken());
-            boolean exist = false;
-            
-            // 이미 있는 학생인지 확인
-            for (Info info : photos) {
-                if (info.number == num) {
+
+            boolean flag = false;
+            for(Info info : list){
+                if(info.number == num){
+                    flag = true;
                     info.recommend++;
-                    exist = true;
-                    break;
                 }
             }
-            
-            // 새로운 학생을 추가해야 하는 경우
-            if (!exist) {
-                if (photos.size() >= n) {
-                    // 가장 적은 추천 수를 가진 학생 중 가장 오래된 학생 제거
-                    Collections.sort(photos);
-                    photos.remove(0);
-                }
-                photos.add(new Info(num, time));
+            Collections.sort(list);
+            if(!flag && list.size() >= n){
+                list.remove(0);
+                list.add(new Info(num, i));
+            }
+            if(!flag && list.size() < n){
+                list.add(new Info(num, i));
             }
         }
-        
-        // 결과 출력
-        List<Integer> result = new ArrayList<>();
-        for (Info info : photos) {
-            result.add(info.number);
+        List<Integer> answer = new ArrayList<>();
+//        int[] answer = new int[n];
+
+        for(Info info : list){
+            answer.add(info.number);
         }
-        Collections.sort(result);
-        
-        for (int num : result) {
-            System.out.print(num + " ");
+        Collections.sort(answer);
+        for(Integer in : answer){
+            System.out.println(in);
         }
+
     }
 }
