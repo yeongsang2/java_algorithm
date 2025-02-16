02d3253401e0a76450a83b02f82515c28e79beff
Kim Yeong Sang <dudtkd1221@gmail.com>
2025-02-16 15:29:27 +0900

    [Gold V] Title: 인내의 도미노 장인 호석, Time: 324 ms, Memory: 22632 KB, Score: 10 point -BaekjoonHub

---
+++ 백준/Gold/20165. 인내의 도미노 장인 호석/인내의 도미노 장인 호석.java
@@ -0,0 +1,104 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n,m,r;
+    static int answer = 0;
+    static int[][] map;
+    static boolean[][] visited;
+    static int cnt;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+
+        map = new int[n + 1][m + 1];
+        visited = new boolean[n + 1][m + 1];
+        for (int i = 1; i < n + 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1 ; j < m + 1; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                map[i][j] = num;
+            }
+        }
+        if(n == 1 && m == 1){
+            System.out.println("F");
+            return;
+        }
+
+        while(r-- > 0){
+            st = new StringTokenizer(br.readLine());
+            // offense
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            String dir = st.nextToken();
+            int height = map[r][c];
+            cnt = 0;
+            if(!visited[r][c]){
+                visited[r][c] = true;
+                dfs(r, c, height, dir);
+            }
+            answer += cnt;
+            // defense
+            st = new StringTokenizer(br.readLine());
+            r = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
+            visited[r][c] = false;
+        }
+        System.out.println(answer);
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if(!visited[i][j]) System.out.print("S ");
+                else System.out.print("F ");
+            }
+            System.out.println();
+        }
+
+    }
+    static void dfs(int r, int c, int height, String dir){
+
+        cnt++;
+//        System.out.println("좌표: " + r + " " + c);
+        // 동 서 남 북
+        if(dir.equals("E")){
+            for (int i = c + 1; i < c + height ; i++) {
+                if(i > m) break;
+                if(!visited[r][i]){
+                    visited[r][i] = true;
+                    dfs(r, i, map[r][i], dir);
+                }
+            }
+        }else if(dir.equals("W")){
+            for (int i = c - 1; i > c - height ; i--) {
+                if(i < 1) break;
+                if(!visited[r][i]) {
+                    visited[r][i] = true;
+                    dfs(r, i, map[r][i], dir);
+                }
+            }
+        }else if(dir.equals("S")){
+            for (int i = r + 1; i < r + height ; i++) {
+                if(i > n) break;
+                if(!visited[i][c]){
+                    visited[i][c] = true;
+                    dfs(i, c, map[i][c], dir);
+                }
+            }
+        }else{
+            for (int i = r - 1; i > r - height ; i--) {
+                if(i < 1) break;
+                if(!visited[i][c]){
+                    visited[i][c] = true;
+                    dfs(i, c, map[i][c], dir);
+                }
+            }
+        }
+    }
+}
