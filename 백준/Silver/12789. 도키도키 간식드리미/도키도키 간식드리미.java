0dd083bcb2ab2c6ac72fe6c953d05ad772ab06b9
Kim Yeong Sang <dudtkd1221@gmail.com>
2025-02-23 16:11:00 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 112 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -1,7 +1,7 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -19,20 +19,25 @@ public class Main {
         }
 
 
-
-        for (int i = 0; i < n - 2; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-                if(arr[i] < arr[j]){
-                    for (int k = j + 1; k < n; k++) {
-                        if( arr[i] > arr[k] ){
-                            System.out.println("Sad");
-                            return;
-                        }
-                    }
+        Stack<Integer> stack = new Stack<>();
+        int point = 1;
+        for (int i = 0; i < n; i++) {
+            int now = arr[i];
+            if( now == point ) {
+                point++;
+                while(!stack.isEmpty() && stack.peek() == point){
+                    stack.pop();
+                    point++;
                 }
+            }else {
+                stack.add(now);
             }
         }
-        System.out.println("Nice");
 
+        if(!stack.isEmpty()){
+            System.out.println("Sad");
+        }else{
+            System.out.println("Nice");
+        }
     }
 }
