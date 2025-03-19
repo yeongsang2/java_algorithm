db14a9c222c3f5044e2a53e8a26a6cf08b372baa
Kim Yeong Sang <dudtkd1221@gmail.com>
2025-03-19 16:25:29 +0900

    [Gold IV] Title: 여행 가자, Time: 156 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -6,45 +6,33 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+    static int[] parent;
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
 
         int[][] map = new int[n + 1][n + 1];
-        int[][] answer = new int[n + 1][n + 1];
+        parent = new int[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            parent[i] = i;
+        }
         for (int i = 1; i < n + 1; i++) {
             st =  new StringTokenizer(br.readLine());
             for (int j = 1; j < n + 1; j++) {
                 int num = Integer.parseInt(st.nextToken());
-                map[i][j] = num;
-                answer[i][j] = num;
-            }
-        }
-
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < n + 1; j++) {
-                if(i != j && answer[i][j] == 0){
-                    answer[i][j] = 100001;
-                }
-            }
-        }
-
-        for (int i = 1; i < n + 1; i++) { // 경유지
-            for (int j = 1; j < n + 1; j++) { // 시작
-                for (int k = 1; k < n + 1; k++) { // 도착
-                    answer[j][k] = Math.min(answer[j][k], answer[j][i] + answer[i][k]);
+                if(num == 1){ //
+                    int rootA = findRoot(i);
+                    int rootB = findRoot(j);
+                    if(rootA == rootB) continue;
+                    union(rootA, rootB);
                 }
             }
         }
 
 //        for (int i = 1; i < n + 1; i++) {
-//            for (int j = 1; j < n + 1; j++) {
-//                System.out.print(answer[i][j] + " ");
-//            }
-//            System.out.println();
+//            System.out.print(parent[i] + " ");
 //        }
 
         st = new StringTokenizer(br.readLine());
@@ -57,12 +45,28 @@ public class Main {
         for (int i = 0; i < m - 1; i++) {
             int now = trip[i];
             int next = trip[i+1];
-            if(answer[now][next] >= 100001){
+            if(findRoot(now) != findRoot(next)){
                 flag = false;
                 break;
             }
         }
         if(flag) System.out.println("YES");
         else System.out.println("NO");
+
+    }
+    static int findRoot(int node){
+        if(parent[node] == node){
+            return node;
+        }
+        parent[node] = findRoot(parent[node]);
+        return parent[node];
+    }
+
+    static void union(int a, int b){
+        if(a <= b){
+            parent[b] = a;
+        }else {
+            parent[a] = b;
+        }
     }
 }
