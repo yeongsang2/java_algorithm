c9f39207ccff55aad54c6c6e177b5e4f4c4ddefd
Kim Yeong Sang <dudtkd1221@gmail.com>
2025-03-20 11:00:25 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 252 ms, Memory: 24944 KB -BaekjoonHub

--- 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.java
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.java
@@ -18,44 +18,30 @@ public class Main {
         for (int i = 0; i < m; i++) {
             loc[i] = Integer.parseInt(st.nextToken());
         }
-        int min = 0;
+        int left = 0;
+        int right = n;
 
-        for (int i = 0; i < m - 1; i++) {
-            int now = loc[i];
-            int next = loc[i + 1];
-            int diff = 0;
-            if( (next - now) % 2 == 0 ){
-                diff = (next - now) / 2;
-            }else{
-                diff = (next - now) / 2  + 1;
+        while (left < right){
+            int mid = (left + right) / 2;
+            if(isPossible(loc, n, mid)){
+                right = mid;
+            }else{ // 높이 높이기
+                left = mid + 1;
             }
-            min = Math.max(min, diff);
         }
+        System.out.println(left);
 
-        min = Math.max(min, (loc[0] - 0));
-        min = Math.max(min, (n - loc[m - 1]));
-        System.out.println(min);
     }
     static boolean isPossible(int[] loc, int n , int range){
 
-        boolean[] checked = new boolean[n + 1];
+        int prev = 0;
 
-        for(int i : loc) {
-
-            for (int j = i - range; j < i + range + 1; j++) {
-                if (j >= 0 && j < n + 1 && !checked[j])
-                    checked[j] = true;
-            }
+        for (int i = 0; i < loc.length; i++) {
+            if(loc[i] - range <= prev){
+                prev = loc[i] + range;
+            }else return false;
         }
 
-        boolean flag = true;
-        for (int i = 0; i < n + 1; i++) {
-            if(!checked[i]){
-                flag = false;
-                break;
-            }
-        }
-        if(!flag) return false;
-        return true;
+        return n - prev <= 0 ;
     }
 }
