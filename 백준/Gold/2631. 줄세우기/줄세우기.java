72d578512ee383fc0b4c4cdf6a299552d3854eea
Kim Yeong Sang <dudtkd1221@gmail.com>
2025-03-23 23:54:12 +0900

    [Gold IV] Title: 줄세우기, Time: 100 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -0,0 +1,38 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = i - 1; j >= 0 ; j--) {
+                if(arr[i] > arr[j]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(n - max);
+    }
+}
