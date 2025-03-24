4fc94d34f8017d5a24cc4522be068ddee268ad0c
Kim Yeong Sang <dudtkd1221@gmail.com>
2025-03-24 11:23:12 +0900

    [Gold III] Title: 불!, Time: 480 ms, Memory: 73772 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -1,25 +1,29 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int r, c;
-    static char[][] map;
-    static int[] dr = {-1, 1, 0, 0};
-    static int[] dc = {0, 0, -1, 1};
 
-    static class Node {
-        int row, col, time;
+    static int r,c;
+    static char[][] map;
+    static int[] dr = {-1,1,0,0};
+    static int[] dc = {0,0,-1,1};
+    static class Point{
+        int r;
+        int c;
+        int time;
         boolean isFire;
 
-        Node(int row, int col, int time, boolean isFire) {
-            this.row = row;
-            this.col = col;
+        public Point(int r, int c, int time, boolean isFire){
+            this.r = r;
+            this.c = c;
             this.time = time;
             this.isFire = isFire;
         }
     }
-
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -27,60 +31,64 @@ public class Main {
         c = Integer.parseInt(st.nextToken());
 
         map = new char[r][c];
-        Queue<Node> queue = new LinkedList<>();
 
-        int jR = 0, jC = 0;
 
+
+        int jR = 0;
+        int jC = 0;
+        Queue<Point> queue = new LinkedList<>();
         for (int i = 0; i < r; i++) {
-            String line = br.readLine();
+            String s = br.readLine();
             for (int j = 0; j < c; j++) {
-                map[i][j] = line.charAt(j);
-                if (map[i][j] == 'J') {
+                char temp = s.charAt(j);
+                if(temp == 'J'){
                     jR = i;
                     jC = j;
-                } else if (map[i][j] == 'F') {
-                    queue.add(new Node(i, j, 0, true)); // 불 먼저 큐에 삽입
                 }
+                if(temp == 'F'){
+                    queue.add(new Point(i, j, 0, true));
+                }
+                map[i][j] = temp;
             }
         }
 
-        // 지훈도 큐에 삽입 (불보다 나중에 실행되도록)
-        queue.add(new Node(jR, jC, 0, false));
+        queue.add(new Point(jR, jC, 0, false));
+        int answer = bfs(queue, map);
 
-        int result = bfs(queue);
-        System.out.println(result == -1 ? "IMPOSSIBLE" : result);
+        if(answer == -1){
+            System.out.println("IMPOSSIBLE");
+        }else{
+            System.out.println(answer);
+        }
     }
 
-    static int bfs(Queue<Node> queue) {
-        while (!queue.isEmpty()) {
-            Node cur = queue.poll();
+    static int bfs(Queue<Point> queue, char[][] map){
 
-            // 지훈이 탈출했을 경우
-            if (!cur.isFire && (cur.row == 0 || cur.row == r - 1 || cur.col == 0 || cur.col == c - 1)) {
-                return cur.time + 1;
+        while (!queue.isEmpty()){
+
+            Point poll = queue.poll();
+            if(!poll.isFire && (poll.r == 0 || poll.r == r -1 || poll.c == 0 || poll.c == c - 1) ){
+                return poll.time + 1;
             }
 
             for (int i = 0; i < 4; i++) {
-                int nextR = cur.row + dr[i];
-                int nextC = cur.col + dc[i];
+                int nextR = poll.r + dr[i];
+                int nextC = poll.c  +dc[i];
 
                 if (nextR < 0 || nextR >= r || nextC < 0 || nextC >= c) continue;
                 if (map[nextR][nextC] == '#' || map[nextR][nextC] == 'F') continue;
 
-                // 불 확산
-                if (cur.isFire) {
+                if(poll.isFire){
                     map[nextR][nextC] = 'F';
-                    queue.add(new Node(nextR, nextC, cur.time + 1, true));
-                }
-                // 지훈 이동
-                else {
-                    if (map[nextR][nextC] == '.') { 
+                    queue.add(new Point(nextR, nextC, 0, true));
+                }else{
+                    if (map[nextR][nextC] == '.') {
                         map[nextR][nextC] = 'J'; // 방문 처리
-                        queue.add(new Node(nextR, nextC, cur.time + 1, false));
+                        queue.add(new Point(nextR, nextC, poll.time + 1, false));
                     }
                 }
             }
         }
-        return -1; // 탈출 불가능
+        return -1;
     }
 }
