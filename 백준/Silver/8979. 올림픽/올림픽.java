777224baee3af0cfb0d572a49ecaf5e18dd822ab
Kim Yeong Sang <dudtkd1221@gmail.com>
2025-05-20 10:15:51 +0900

    [Silver V] Title: 올림픽, Time: 104 ms, Memory: 14084 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -0,0 +1,81 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static class Team implements Comparable<Team> {
+
+        int teamNumber;
+        int gold;
+        int silver;
+        int brown;
+        public Team(int teamNumber, int gold, int silver, int brown){
+            this.teamNumber = teamNumber;
+            this.gold = gold;
+            this.silver = silver;
+            this.brown = brown;
+        }
+
+        @Override
+        public int compareTo(Team o){
+            if(this.gold == o.gold){
+                if(this.silver == o.silver){
+                    return o.brown - this.brown;
+                }
+                return o.silver - this.silver;
+            }
+            return o.gold - this.gold;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+        int teamCnt = Integer.parseInt(st.nextToken());
+        int targetTeam = Integer.parseInt(st.nextToken());
+
+        List<Team> list = new ArrayList<>();
+
+        for (int i = 0; i < teamCnt; i++) {
+            st = new StringTokenizer(br.readLine());
+            int teamN = Integer.parseInt(st.nextToken());
+            int g = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list.add(new Team(teamN, g, s, b));
+        }
+
+        Collections.sort(list);
+
+        int order = 0;
+        Team prevTeam = null;
+        for (int i = 0; i < teamCnt; i++) {
+            Team team = list.get(i);
+            if(prevTeam != null){
+                if(prevTeam.gold == team.gold && prevTeam.silver == team.silver && prevTeam.brown == team.brown){
+                    ;
+                }else{
+
+                    order++;
+                }
+            }else{
+                order++;
+            }
+            prevTeam = team;
+            if(team.teamNumber == targetTeam){
+                System.out.println(order);
+            }
+
+        }
+
+    }
+}
